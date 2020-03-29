c4e47a87b23af3663b2514efd81a160996eaf032
esteban.adrian.159@gmail.com <alexhuerta147@gmail.com>
2020-03-28 22:18:24 -0500

    cambios en reconocimiento

---
+++ app/src/main/java/com/rozvi14/facialrecognition/utils/UtilMethods.java
@@ -0,0 +1,25 @@
+package com.rozvi14.facialrecognition.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
+public class UtilMethods {
+    public static Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        Matrix matrix = new Matrix();
+        matrix.postRotate(-90);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(image, width, height, true);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+        return rotatedBitmap;
+    }
+}
